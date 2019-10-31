86a63458238267dcb081b17c74f699867bf22f25
Ethan Robish <ethan.robish@gmail.com>
2019-10-31 13:06:46 -0500

    Revert "Disable copy button on inline blocks"
    This reverts commit 3acfde2adbcd39b8ccc624c249ca5eedb7fa4559.

--- themes/hugo-theme-learn/static/js/learn.js
+++ themes/hugo-theme-learn/static/js/learn.js
@@ -175,12 +175,11 @@ jQuery(document).ready(function() {
 
     // clipboard
     var clipInit = false;
-    // only copy button add to full ``` blocks and not inline code blocks.
-    $('pre code').each(function() {
+    $('code').each(function() {
         var code = $(this),
             text = code.text();
 
-        // if (text.length > 5) {
+        if (text.length > 5) {
             if (!clipInit) {
                 var text, clip = new ClipboardJS('.copy-to-clipboard', {
                     text: function(trigger) {
@@ -211,7 +210,7 @@ jQuery(document).ready(function() {
             code.next('.copy-to-clipboard').on('mouseleave', function() {
                 $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
             });
-        // }
+        }
     });
 
     // allow keyboard control for prev/next links
@@ -230,7 +229,7 @@ jQuery(document).ready(function() {
              e.stopPropagation();
          }
      });
-
+    
     jQuery(document).keydown(function(e) {
       // prev links - left arrow key
       if(e.which == '37') {
@@ -265,7 +264,7 @@ jQuery(document).ready(function() {
         });
     }
 
-    /**
+    /** 
     * Fix anchor scrolling that hides behind top nav bar
     * Courtesy of https://stackoverflow.com/a/13067009/28106
     *
@@ -347,7 +346,7 @@ jQuery(document).ready(function() {
 
         $(document).ready($.proxy(anchorScrolls, 'init'));
     })(window.document, window.history, window.location);
-
+    
 });
 
 jQuery(window).on('load', function() {
